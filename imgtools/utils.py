293d4a0b8d2db3f7f3c82e0bf18859b13b954a6e
Kenta Fukuhara <105337464+KAkikakuFukuhara@users.noreply.github.com>
2023-03-23 19:53:37 +0900

    [Add] tools/resize.py
    [Delete] tools/png2jpeg_with_resize.py
    
    [Update] arg help
    
    [Add] tools/resize.py into tools/__main__.py

--- imgtools/utils.py
+++ imgtools/utils.py
@@ -1,21 +1,41 @@
-from typing import Union
+from typing import Any,Union
 from pathlib import Path
 
 
-def check_path_exists(path:Union[str, Path]):
+def can_cvt_int(any:Any) -> bool:
+    try:
+        int(round(float(any), 0))
+        return True
+    except:
+        return False
+
+
+def check_can_cvt_int(any:Any):
+    assert can_cvt_int(any), f"Can't convrt {any} to int"
+
+
+def is_positive(num:int) -> bool:
+    return True if num > 0 else False
+
+
+def check_is_positive(num:int):
+    assert can_cvt_int(num), f"{num} isn't positive"
+
+
+def check_is_valid_path(path:Union[str, Path]):
     path_:Path = Path(path)
     assert path_.exists(), f"Not Found {Path}"
 
 
-def check_dir(path:Union[str, Path]):
+def check_is_valid_dir(path:Union[str, Path]):
     path_:Path = Path(path)
-    check_path_exists(path_)
+    check_is_valid_path(path_)
     assert path_.is_dir(), f"{path_} is not directory"
 
 
-def check_file(path:Union[str, Path]):
+def check_is_valid_file(path:Union[str, Path]):
     path_:Path = Path(path)
-    check_path_exists(path_)
+    check_is_valid_path(path_)
     assert path_.is_file(), f"{path_} is not file"
 
 
